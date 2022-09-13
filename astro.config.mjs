49adf7965ade7747b88958204f035898d1ba2720
Sohail M. Khan <sokhan@mediamath.com>
2022-09-13 18:21:20 +0800

    Partytown and gtag.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import deno from "@astrojs/deno";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: deno()
+  adapter: deno(),
+  integrations: [partytown({
+    // Adds dataLayer.push as a forwarding-event.
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  })]
 });
